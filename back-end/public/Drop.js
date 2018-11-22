e4cc80963bc6c548b40ea6d53591c6b4e83ff61d
Dellamoresteven <dellamoresteven@gmail.com>
2018-11-21 18:20:25 -0600

    Bullets, multplayer, drops

---
+++ back-end/public/Drop.js
@@ -0,0 +1,9 @@
+class Drop{
+	constructor(data){
+		// {type: 2, rare: 1, locationX: 304, locationY: 138}
+		this.type = data.type;
+		this.rare = data.rare;
+		this.x = data.locationX;
+		this.y = data.locationY;
+	}
+}
