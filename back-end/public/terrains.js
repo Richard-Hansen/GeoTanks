231bed681f6c9489c37d333dba0f108ce7744f92
Richard Hansengit config --global user.name Richard <richie.hansen@gmail.com>
2018-12-02 19:14:28 -0500

    Added all collision functionality

---
+++ back-end/public/terrains.js
@@ -0,0 +1,24 @@
+// "_id": 0,
+// "name": "Basic",
+// "dmg": 5,
+// "speed": 10,
+// "distance": 600,
+// "cd": 500,
+// "auto": false,
+// "size": 10,
+// "hitbox": 50,
+// "iconname": "",
+// "iconf
+
+
+
+class terrain {
+    constructor(type, x, y, hitbox) {
+        // console.log(type + ":" + x + ":" + y + ":" + intervalX + ":" + intervalY)
+        // imageMode(CENTER);
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.hitbox = hitbox;
+    }
+}
